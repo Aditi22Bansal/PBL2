f682aa841c136faf50a079f4ad616db9a98cf21b
aditi-bansal22 <aditi.dev@projecthumancity.com>
2026-04-10 11:40:32 +0530

    Fix Axios Unsupported Protocol Error by forcing URLs to contain https://

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,1 +1,5 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+let _url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+if (!_url.startsWith("http")) {
+  _url = "https://" + _url;
+}
+export const API_URL = _url;
